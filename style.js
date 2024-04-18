4c0be627cd9f4c225e93e951bc1427f918181503
Kim Vo <126284919+KimVVo@users.noreply.github.com>
2024-04-18 14:55:08 -0500

    Create style.js

---
+++ style.js
@@ -0,0 +1,6 @@
+// Add your custom JavaScript functionality here
+
+// Example: Add a click event listener to the header logo
+document.querySelector('header').addEventListener('click', function() {
+    alert('You clicked the header!');
+});
